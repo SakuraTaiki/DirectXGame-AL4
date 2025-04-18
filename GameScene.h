39b36dd794710bf351ae0cd3c9d319d143c31658
SakuraTaiki <k024g0010@g.neec.ac.jp>
2025-04-18 13:03:24 +0900

    モデルの生成　デバッグの表示

--- GameScene.h
+++ GameScene.h
@@ -7,12 +7,24 @@ class GameScene
 public:
 	//初期化
 	void Initialize();
-	uint32_t textureHandle_ = 0;
-	KamataEngine::Sprite* sprite_ = nullptr;
+
+	
 
 	//更新
 	void Update();
 
 	//描画
 	void Draw();
+	~GameScene();
+
+private:
+	uint32_t textureHandle_ = 0;
+	uint32_t soundDateHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+	KamataEngine::Sprite* sprite_ = nullptr;
+	KamataEngine::Model* model_ = nullptr;
+	KamataEngine::WorldTransform worldTransform_;
+	KamataEngine::Camera camera_;
+	KamataEngine::DebugCamera* debugcamera_ = nullptr;
+	float inputFloat3[3] = {0, 0, 0};
 };
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include"KamataEngine.h"
 #include"GameScene.h"
+
 using namespace KamataEngine;
 // Windowsアプリでのエントリーポイント(main関数)
 
@@ -17,21 +18,28 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	//ゲームシーンの初期化
 	gameScene->Initialize();
 
+	ImGuiManager* imguiManager = ImGuiManager::GetInstance();
 	//メインループ
 	while (true) {
 		// エンジンの更新
 		if (KamataEngine::Update()) {
 			break;
 		}
-
+		//imGui受付開始
+		imguiManager->Begin();
 		//ゲームシーンの更新
 		gameScene->Update();
-
+		//imGui受付終了
+		imguiManager->End();
 		// 描画開始
 		dxCommon->PreDraw();
 
 		//ゲームシーンの描画
 		gameScene->Draw();
+		//軸表示の描画
+		AxisIndicator::GetInstance()->Draw();
+		//imGui描画
+		imguiManager->Draw();
 
 		// 描画終了
 		dxCommon->PostDraw();
